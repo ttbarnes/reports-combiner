0edd9937a2d1954467f7a8ddfa45a8579899ef1d
ttbarnes <tony@tonybarnes.me>
2018-01-27 17:18:52 +0000

    render table with mock data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import MOCK_DATA from './mockData';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>hello</p>
+        <h1>Reports Combiner</h1>
+
+        <table>
+          <thead>
+            <tr>
+              {MOCK_DATA.headings.map((cell, i) =>
+                <td
+                  key={cell + i}
+                >
+                  {cell}
+                </td>
+              )}
+            </tr>
+          </thead>
+          <tbody>
+            {MOCK_DATA.rows.map((row, i) =>
+              <tr key={row + i}>
+                {row.map((c) =>
+                  <td key={c + i}>
+                    {c}
+                  </td>
+                )}
+              </tr>
+            )}
+          </tbody>
+        </table>
+
       </div>
     );
   }
